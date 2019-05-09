ad12f0c0668130eaea1c9cab2cb8728909b765cb
loulou <loulou.pei@hotmail.com>
2019-05-09 12:08:48 +0200

    fix login screen html + css

--- src/EventAlbum/EventAlbum.js
+++ src/EventAlbum/EventAlbum.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "../index.css";
 import "./EventAlbum.css";
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -9,7 +10,7 @@ class EventAlbum extends Component {
       <div className="EventAlbum">
         <p>EventAlbum</p>
 
-      
+
       </div>
     );
   }
--- src/LogIn/LogIn.js
+++ src/LogIn/LogIn.js
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "./LogIn.css";
 import 'bootstrap/dist/css/bootstrap.css';
+import "../index.css";
+import "./LogIn.css";
+import "../data/Model.js"
 
 class LogIn extends Component {
+
   render() {
+    function hejhek(name) {
+      return <h1>{name}</h1>
+    }
     return (
       <div className="LogIn">
-        <p>Log in to be able to join events like Jens Mega Party!</p>
-
-        <Link to="/Home">
-          <button>Log in</button>
-        </Link>
+        <div id="outer-box">
+          <h1>PhotoShare.</h1>
+          <div class="horizontal-line"></div>
+          <div id="leftalign">
+            <p> Capture the moment with our revolutionizing event-based photo sharing platform</p>
+          </div>
+          <br/>
+          <input type="email" placeholder="Email" name="email" required></input><br/>
+          <input type="password" placeholder="Password" name="psw" required></input>
+          <br/>
+          <br/>
+          <Link to="/Home">
+            <button type="button" class="btn">Log In</button>
+          </Link>
+        </div>
+        <div class="footer">
+          <p>Don't have an account?<span class="bold"> Sign Up</span></p>
+        </div>
       </div>
+
     );
   }
 }
